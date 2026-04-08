6321cb4e79ea7899b66d9064fe9d34f0064e0ce0
LucasJen <fake@fake.com>
2026-04-08 18:58:50 -0500

    project init and bmi calc start

---
+++ bmi-calculator-components/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import BodyMassIndexForm from './components/BodyMassIndexForm.vue';
+
+</script>
+
+<template>
+  <h1>This is App.vue</h1>
+
+  <BodyMassIndexForm v-on:stats-entered="calculateBmi"/>
+</template>
---
+++ bmi-calculator-components/src/components/BodyMassIndexForm.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref, defineEmits } from 'vue'
+
+const emi = defineEmits(['stats-entered'])
+
+const height = ref(0)
+const weight = ref(0)
+
+function stats-entered() {
+    // this is called when the button.
+    // need to emit an event including the height and weight.
+    // the event will be received by the parent compoent of this component.
+    // in this app, that's App.vue.
+    // in another app, it's whatever the parent is.
+    // this component doesn't know and doesn't care where the even goes.
+    emit('stats-entered')
+
+}
+
+</script>
+
+<template>
+
+    <h2>This is the BMI page</h2>
+
+    <label for="height">Height in meters</label><input id="height" v-model="height"/>
+    <label for="weight">Weight in kilograms</label><input id="weight" v-model="weight"/>
+
+    <button v-on:click="calculateBmi">Calculate!</button>
+
+</template>
+
+<style scoped>
+
+</style>
