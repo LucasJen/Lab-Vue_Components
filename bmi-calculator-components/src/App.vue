f300397442a8bcf4c7fd2ea765915991ee4ac035
LucasJen <fake@fake.com>
2026-04-08 19:21:09 -0500

    moved function to app.vue

--- bmi-calculator-components/src/App.vue
+++ bmi-calculator-components/src/App.vue
@@ -1,5 +1,15 @@
 <script setup>
+import { ref } from 'vue'
+
 import BodyMassIndexForm from './components/BodyMassIndexForm.vue';
+
+const bmi = ref() // Falsy value
+
+function calculateBmi(height, weight) {
+
+  let bmiValue = weight / (height * height)
+  bmi.value = bmiValue
+}
 
 </script>
 
@@ -7,4 +17,8 @@ import BodyMassIndexForm from './components/BodyMassIndexForm.vue';
   <h1>This is App.vue</h1>
 
   <BodyMassIndexForm v-on:stats-entered="calculateBmi"/>
+
+  <p v-if="bmi">Your BMI is: {{ bmi }}</p>
+  <p v-else>Please enter data in the form</p>
+
 </template>
--- bmi-calculator-components/src/components/BodyMassIndexForm.vue
+++ bmi-calculator-components/src/components/BodyMassIndexForm.vue
@@ -1,20 +1,19 @@
 <script setup>
 import { ref, defineEmits } from 'vue'
 
-const emi = defineEmits(['stats-entered'])
+const emit = defineEmits(['stats-entered'])
 
 const height = ref(0)
 const weight = ref(0)
 
-function stats-entered() {
+function statsEntered() {
     // this is called when the button.
     // need to emit an event including the height and weight.
     // the event will be received by the parent compoent of this component.
     // in this app, that's App.vue.
     // in another app, it's whatever the parent is.
     // this component doesn't know and doesn't care where the even goes.
-    emit('stats-entered')
-
+    emit('stats-entered', height.value, weight.value)
 }
 
 </script>
@@ -26,7 +25,7 @@ function stats-entered() {
     <label for="height">Height in meters</label><input id="height" v-model="height"/>
     <label for="weight">Weight in kilograms</label><input id="weight" v-model="weight"/>
 
-    <button v-on:click="calculateBmi">Calculate!</button>
+    <button v-on:click="statsEntered">Calculate!</button>
 
 </template>
 
